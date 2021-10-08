24e63cc153a0d439bc4c34ce9af1a73e7586bb50
javi <javi.cazalla@gmail.com>
2021-10-08 18:40:44 +0200

    Formulario2.php listo para entregar

---
+++ Formularios/formulario2.md
@@ -0,0 +1,24 @@
+# Primer  Formulario
+
+Este proyecto es un formulario hecho por mí. El funcionamiento aun es muy básico ya que solamente valida y mantiene los resultados en el campo correspondiente. Pero es el primer paso para empezar cada vez a hacer cosas más elaboradas y con más funcionalidad.
+
+#### Pre-requisitos
+
+Necesitaremos un entorno de desarrollo web como puede ser Wampserver que se puede encontrar en Internet. Ya que este tendrá un interprete de PHP que podrá interpretar el código generado por nosotros.
+
+## Ejecutando las pruebas
+
+Debemos de arrancar nuestro entorno de desarrollo web y meter el archivo formulario2.php en la carpeta donde alojemos las páginas web que queremos lanzar una vez ahí lanzaremos la página desde el navegador web y nos cargará el formulario. Ya podremos usar el formulario tal y como queramos.
+
+## Construido con
+
+- HTML - Lenguaje usado para crear la web 
+
+- PHP - Lenguaje usado para dar funcionalidad
+
+  ## 
+
+## Autores
+
+- Javier Cazalla Estrella - Trabajo y documentación
+
--- Formularios/formulario2.php
+++ Formularios/formulario2.php
@@ -16,7 +16,15 @@
     ?>
     <form action="" method="post">
         <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" value="<?php echo isset($errores['nombre']) ? '' : $_POST['nombre']; ?>" />
+        <input type="text" name="nombre" value="<?php
+                                                if (isset($errores['nombre'])) {
+                                                    echo '';
+                                                } else if (isset($_POST['nombre'])) {
+                                                    echo $_POST['nombre'];
+                                                } else {
+                                                    echo '';
+                                                }
+                                                ?>" />
         <p><span style="color:red">
                 <?php
                 echo isset($errores['nombre']) ? $errores['nombre'] : '';
@@ -24,7 +32,15 @@
             </span></p>
         <br />
         <label for="apellidos">Apellidos</label>
-        <input type="text" name="apellidos" value="<?php echo isset($errores['apellidos']) ? '' : $_POST['apellidos']; ?>" />
+        <input type="text" name="apellidos" value="<?php
+                                                    if (isset($errores['apellidos'])) {
+                                                        echo '';
+                                                    } else if (isset($_POST['apellidos'])) {
+                                                        echo $_POST['apellidos'];
+                                                    } else {
+                                                        echo '';
+                                                    }
+                                                    ?>" />
         <p><span style="color:red">
                 <?php
                 echo isset($errores['apellidos']) ? $errores['apellidos'] : '';
@@ -32,7 +48,15 @@
             </span></p>
         <br />
         <label for="DNI">DNI</label>
-        <input type="text" name="DNI" value="<?php echo isset($errores['DNI']) ? '' : $_POST['DNI']; ?>" />
+        <input type="text" name="DNI" value="<?php
+                                                if (isset($errores['DNI'])) {
+                                                    echo '';
+                                                } else if (isset($_POST['DNI'])) {
+                                                    echo $_POST['DNI'];
+                                                } else {
+                                                    echo '';
+                                                }
+                                                ?>" />
         <p><span style="color:red">
                 <?php
                 echo isset($errores['DNI']) ? $errores['DNI'] : '';
@@ -40,7 +64,15 @@
             </span></p>
         <br />
         <label for="fechaNac">Fecha nacimiento</label>
-        <input type="text" name="fechaNac" value="<?php echo isset($errores['fechaNac']) ? '' : $_POST['fechaNac']; ?>" />
+        <input type="text" name="fechaNac" value="<?php
+                                                    if (isset($errores['fechaNac'])) {
+                                                        echo '';
+                                                    } else if (isset($_POST['fechaNac'])) {
+                                                        echo $_POST['fechaNac'];
+                                                    } else {
+                                                        echo '';
+                                                    }
+                                                    ?>" />
         <p><span style="color:red">
                 <?php
                 echo isset($errores['fechaNac']) ? $errores['fechaNac'] : '';
@@ -48,7 +80,15 @@
             </span></p>
         <br />
         <label for="correo">Correo electr&oacute;nico</label>
-        <input type="text" name="correo" value="<?php echo isset($errores['correo']) ? '' : $_POST['correo']; ?>" />
+        <input type="text" name="correo" value="<?php
+                                                if (isset($errores['correo'])) {
+                                                    echo '';
+                                                } else if (isset($_POST['correo'])) {
+                                                    echo $_POST['correo'];
+                                                } else {
+                                                    echo '';
+                                                }
+                                                ?>" />
         <p><span style="color:red">
                 <?php
                 echo isset($errores['correo']) ? $errores['correo'] : '';
@@ -56,12 +96,32 @@
             </span></p>
         <br />
         <label for="web">P&aacute;gina web</label>
-        <input type="text" name="web" value="<?php echo isset($errores['web']) ? '' : $_POST['web']; ?>" />
+        <input type="text" name="web" value="<?php
+                                                if (isset($errores['web'])) {
+                                                    echo '';
+                                                } else if (isset($_POST['web'])) {
+                                                    echo $_POST['web'];
+                                                } else {
+                                                    echo '';
+                                                }
+                                                ?>" />
         <p><span style="color:red">
                 <?php
                 echo isset($errores['web']) ? $errores['web'] : '';
                 ?>
             </span></p>
+        <br />
+        <input type="checkbox" name="zumos[]" value="piña" <?php
+                                                            if (isset($_POST['zumos']) && in_array("piña", $_POST['zumos']))
+                                                                echo 'checked="checked"';
+                                                            ?> />
+        Zumo de pi&ntilde;a
+        <br />
+        <input type="checkbox" name="zumos[]" value="naranja" <?php
+                                                                if (isset($_POST['zumos']) && in_array("naranja", $_POST['zumos']))
+                                                                    echo 'checked="checked"';
+                                                                ?> />
+        Zumo de naranja
         <br />
         <button type="submit" name="enviar">Enviar</button>
     </form>
