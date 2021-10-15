2fde5a5965c80554d66f7cb59856c45572586618
Javier Cazalla Estrella <jcazest2202@g.educaand.es>
2021-10-15 09:53:31 +0200

    Trabajo clase dia 15 de octubre 2021

--- DB/dbProductos.php
+++ DB/dbProductos.php
@@ -1,11 +1,25 @@
 <?php
 include("../Entidades/Producto.php");
-class ProductosDB{
-   public static $productos=array(
-       Producto[]
-   );
+class ProductosDB
+{
+    public static $productos = array();
 
-   public static function obtieneProductos(){
-       return $productos;
-   }
+    public static function obtieneProductos()
+    {
+        return self::$productos;
+    }
+
+    public static function obtieneProductoCodigo($codigo)
+    {
+        if (isset($productos[$codigo])) {
+            return self::$productos[$codigo];
+        }
+    }
+
+    public static function borraProducto($codigo)
+    {
+        if (isset($productos[$codigo])) {
+            unset($productos[$codigo]);
+        }
+    }
 }
