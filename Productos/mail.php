5db027bcaff54fe4777c2b7074fb0464ebeffce8
Javier Cazalla Estrella <jcazest2202@g.educaand.es>
2021-11-24 10:18:52 +0100

    Php mailer prueba

---
+++ Productos/mail.php
@@ -0,0 +1,39 @@
+<?php
+$rutaImagen = "dogo-aleman.jpg";
+$contenidoBinario = file_get_contents($rutaImagen);
+$imagenComoBase64 = base64_encode($contenidoBinario);
+$foto= "<img src='data:image/jpeg;base64,".$imagenComoBase64."'/>";
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require "vendor/autoload.php";
+$mail = new PHPMailer();
+$mail->IsSMTP();
+// cambiar a 0 para no ver mensajes de error
+$mail->SMTPDebug  = 2;
+$mail->SMTPAuth   = true;
+$mail->SMTPSecure = "tls";
+$mail->Host       = "smtp.gmail.com";
+$mail->Port       = 587;
+// introducir usuario de google
+$mail->Username   = "javiCazallaPruebas@gmail.com";
+// introducir clave
+$mail->Password   = "alcantarilla";
+$mail->SetFrom('javiCazallaPruebas@gmail.com', 'Test');
+// asunto
+$mail->Subject    = "Foto enviada";
+// cuerpo
+$mail->MsgHTML($foto);
+// adjuntos
+//$mail->addAttachment("dogo-aleman.jpg");
+// destinatario
+$address = "jve@ieslasfuentezuelas.com";
+$mail->AddAddress($address, "Test");
+// enviar
+$resul = $mail->Send();
+if (!$resul) {
+    echo "Error" . $mail->ErrorInfo;
+} else {
+    echo "Enviado";
+}
---
+++ Productos/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9b5e444ad8bed3043a29d6e1ad0d1be0ba6f61cc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9b5e444ad8bed3043a29d6e1ad0d1be0ba6f61cc',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.1',
+            'version' => '6.5.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
+            'dev_requirement' => false,
+        ),
+    ),
+);
